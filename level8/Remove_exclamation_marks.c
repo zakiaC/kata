d34bf946e098c8d39fd53faa89ea4d19402eb168
LOUASSA NADIA <louassanadia@MacBook-Pro-de-LOUASSA.local>
2022-08-15 13:47:47 +0200

    new fundamental kata

--- level8/Remove_exclamation_marks.c
+++ level8/Remove_exclamation_marks.c
@@ -1,5 +1,21 @@
 #include<unistd.h>
 
-void remove_exclamation_marks(const char *str_in, char *str_out) {
-
+void remove_exclamation_marks(const char *str_in, char *str_out)
+{
+  int i;
+  int j;
+  
+  i = 0;
+  j = 0;
+  
+  while('\0' != str_in[i])
+  {
+    if('!' != str_in[i])
+    {
+      str_out[j] = str_in[i];
+      ++str_out;
+    }
+    
+    str_in++;
+  }
 }
