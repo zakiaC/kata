632e43bce06378a5918fad544c05ac69f3f37f13
LOUASSA NADIA <louassanadia@MacBook-Pro-de-LOUASSA.local>
2022-08-15 13:51:05 +0200

    correction syntaxe

--- level8/Remove_exclamation_marks.c
+++ level8/Remove_exclamation_marks.c
@@ -8,12 +8,12 @@ void remove_exclamation_marks(const char *str_in, char *str_out)
   i = 0;
   j = 0;
   
-  while('\0' != str_in[i])
+  while(str_in[i] != '\0')
   {
-    if('!' != str_in[i])
+    if(str_in[i] != '!')
     {
       str_out[j] = str_in[i];
-      ++str_out;
+      str_out++;
     }
     
     str_in++;
